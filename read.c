eb88c1a83403cb6c2325631d1acc9593e4f2ccce
Niklas Schnelle <niklas@komani.de>
2012-01-29 00:44:35 +0100

    A simple C code to read from the Arduino tty

---
+++ read.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+int main(int argc, char **argv) {
+
+    int fd;
+    ssize_t len;
+    const unsigned int buflen = 4096;
+    unsigned char buf[buflen];
+    struct termios tio;
+
+    fd = open( argv[1], O_RDONLY | O_NOCTTY | O_NONBLOCK );
+    cfmakeraw(&tio);
+    cfsetispeed(&tio,B9600);
+    cfsetospeed(&tio,B9600);
+    tcsetattr(fd,TCSANOW,&tio);
+
+    for(;;) {
+        len = read( fd, buf, buflen );
+        if( len > 0 ) write(1,buf,len);
+    }
+
+}
